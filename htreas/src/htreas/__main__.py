343958ad352ccc03812dccb42a2c9afddd03c74b
kkorolyov <kirillk753@gmail.com>
2024-11-12 21:23:00 -0800

    stop mucking

--- htreas/src/htreas/__main__.py
+++ htreas/src/htreas/__main__.py
@@ -1,20 +1,23 @@
 import argparse
-from os import environ
 import re
 import subprocess
-
+from os import environ
 
 TREASURY_PREFIX = "TBill"
 
 parser = argparse.ArgumentParser(
     description="Updates treasuries in ledger files",
-    formatter_class=argparse.RawTextHelpFormatter,
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
 parser.add_argument(
-    "-i", "--input", type=str, help="ledger file to read commodities from"
+    "-i",
+    "--input",
+    type=str,
+    default=environ["LEDGER_FILE"],
+    help="ledger file to read",
 )
 parser.add_argument(
-    "-o", "--output", type=str, required=True, help="ledger file to write output to"
+    "-o", "--output", type=str, required=True, help="ledger file to write"
 )
 
 
@@ -37,7 +40,7 @@ def fetchTreasuries(path: str):
 
 
 def formatTreasury(text: str):
-    pattern = re.compile(".*\\((.*) - (.*)\\)")
+    pattern = re.compile(r".*\((.*) - (.*)\)")
 
     match = pattern.search(text)
 
@@ -52,17 +55,14 @@ def formatTreasury(text: str):
 
 def main():
     args = parser.parse_args()
-    input = args.input or environ["LEDGER_FILE"]
-    output = args.output
 
-    commodities = fetchCommodities(input)
-    treasuries = fetchTreasuries(output)
+    commodities = fetchCommodities(args.input)
+    treasuries = fetchTreasuries(args.output)
 
     newTreasuries = [formatTreasury(t) for t in commodities if t not in treasuries]
     newLines = [t for group in newTreasuries for t in group]
 
     print(f"writing {len(newLines)} lines: {newLines}")
-
     if len(newLines):
-        with open(output, "a") as f:
+        with open(args.output, "a") as f:
             f.write("\n".join(["", *newLines, ""]))
